6522d20fa1e1c1dc953fbb291869d390d44940ab
stylliaga <stylliaga@yandex.ru>
2019-05-14 16:50:47 +0300

    Консоль: Многопоточность через Thread
    Наследование класса Thread

--- src/ru/stylliaga/Main.java
+++ src/ru/stylliaga/Main.java
@@ -1,37 +1,18 @@
 package ru.stylliaga;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.BufferedReader;
-import java.io.FileReader;
-
+class  SomeThread extends Thread{
+	public void run(){
+		for(int i = 0; i < 10; i++){
+			System.out.println(i);
+		}
+	}
+}
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-	    BufferedReader br = null;
-    	try {
-			File file = new File("newfile.txt");
+    public static void main(String[] args) {
+		SomeThread test = new SomeThread();
+		test.run();
 
-			if(!file.exists()){
-			file.createNewFile();}
-
-			PrintWriter pw = new PrintWriter(file);
-			pw.println("Все работает.");
-			pw.println("Helolo World!");
-			pw.println("New String");
-			pw.close();
-
-			br = new BufferedReader(new FileReader("newfile.txt"));
-			String line;
-			while ((line = br.readLine()) != null){
-				System.out.println(line);
-			}
-
-		}catch (IOException e){
-	    	System.out.println("Error - " + e);
-		}finally {
-    		br.close();
-		}
+		SomeThread test1 = new SomeThread();
+		test1.run();
     }
 }
