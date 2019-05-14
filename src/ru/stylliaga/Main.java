a9e7ff878f26f185c69137f7681f05afa2989e17
stylliaga <stylliaga@yandex.ru>
2019-05-14 16:18:43 +0300

    Консоль: Отслеждивание исключения
    try - catch

--- src/ru/stylliaga/Main.java
+++ src/ru/stylliaga/Main.java
@@ -5,40 +5,18 @@ import java.util.LinkedList;
 public class Main {
 
     public static void main(String[] args) {
-	    int some[] = new int [4];
+	    int arr[] = {23,6,7};
 
-		ArrayList <Integer> some_dynamic = new ArrayList<>(5);
-		some_dynamic.add(56);
-		some_dynamic.add(6);
-		some_dynamic.add(7);
-		some_dynamic.add(5);
-		some_dynamic.add(536);
-		some_dynamic.add(8);
+    	try{
 
-		some_dynamic.remove(1);
-
-		System.out.println(some_dynamic.size());
-		System.out.println("--------------");
-
-
-		for(Integer x : some_dynamic){
-			System.out.println(x);
-		}
-		LinkedList<String> names = new LinkedList<>();
-		names.add("Tom");
-		names.add("Слай");
-		names.add("Антон");
-		names.add("John");
-
-		names.add(1,"Федя");
-		names.add(0,"Андрюха");
-
-		System.out.println("--------------");
-		System.out.println(names.size());
-		System.out.println("--------------");
-
-		for(String x : names){
-			System.out.println(x);
+			//System.out.println(arr[7]);
+	    	//int x = 100 / 0;
+		}catch (ArithmeticException e){
+	    	System.out.println("Ошибка выполнения операции " + e);
+		}catch (Exception e){
+    		System.out.println("Error - " + e);
+		}finally {
+			System.out.println("Finally is working!");
 		}
     }
 }
