e7a180a0df4114439c5bb338d584d5a95f11010e
stylliaga <stylliaga@yandex.ru>
2019-05-14 16:34:32 +0300

    Консоль: чтение из файла

--- src/ru/stylliaga/Main.java
+++ src/ru/stylliaga/Main.java
@@ -3,10 +3,14 @@ package ru.stylliaga;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    try {
+    public static void main(String[] args) throws IOException {
+	    BufferedReader br = null;
+    	try {
 			File file = new File("newfile.txt");
 
 			if(!file.exists()){
@@ -15,10 +19,19 @@ public class Main {
 			PrintWriter pw = new PrintWriter(file);
 			pw.println("Все работает.");
 			pw.println("Helolo World!");
+			pw.println("New String");
 			pw.close();
+
+			br = new BufferedReader(new FileReader("newfile.txt"));
+			String line;
+			while ((line = br.readLine()) != null){
+				System.out.println(line);
+			}
 
 		}catch (IOException e){
 	    	System.out.println("Error - " + e);
+		}finally {
+    		br.close();
 		}
     }
 }
